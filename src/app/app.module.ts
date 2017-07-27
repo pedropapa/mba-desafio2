7c118a48f393b3786049d9936ff55efaa7c33e77
Pedro Papadópolis <pplp93@gmail.com>
2017-07-27 13:03:42 -0300

    feat(app): Implementação inicial do aplicativo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from "@angular/platform-browser";
+import {ErrorHandler, NgModule} from "@angular/core";
+import {IonicApp, IonicErrorHandler, IonicModule} from "ionic-angular";
+import {SplashScreen} from "@ionic-native/splash-screen";
+import {StatusBar} from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from "./app.component";
+import {HomePage} from "../pages/home/home";
+import {Geolocation} from "@ionic-native/geolocation";
+import {LaunchNavigator} from "@ionic-native/launch-navigator";
 
 @NgModule({
   declarations: [
@@ -24,7 +26,10 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
+    LaunchNavigator,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,78 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, ElementRef, ViewChild} from "@angular/core";
+import {ToastController} from "ionic-angular";
+import {Geolocation, Geoposition} from "@ionic-native/geolocation";
+import {LaunchNavigator} from "@ionic-native/launch-navigator";
+
+declare let google;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  map: any;
+  @ViewChild('mapa') mapElement: ElementRef;
+  devicePosition: Geoposition;
+  failure: string = '';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public toastCtrl: ToastController, public geolocation: Geolocation, public launchNavigator: LaunchNavigator) {
 
   }
 
+  ionViewDidLoad() {
+    this.geolocation.getCurrentPosition().then((position) => {
+      console.log(position);
+      this.devicePosition = position;
+
+      this.loadMap();
+    }, (err) => {
+      this.failure = err.message;
+      this.toastMessage(err.message);
+    });
+  }
+
+  private loadMap() {
+    let latLng = new google.maps.LatLng(this.devicePosition.coords.latitude, this.devicePosition.coords.longitude);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    let marker = new google.maps.Marker({
+      position: latLng,
+      map: this.map,
+      title: 'Clique para Ir'
+    });
+
+    let self = this;
+
+    marker.addListener('click', event => {
+      self.irParaPonto(event)
+    } );
+
+    marker.setMap(this.map);
+  }
+
+  public irParaPonto(event: any) {
+    this.launchNavigator.navigate([event.latLng.lat(), event.latLng.lng()])
+      .then(
+        () => {
+          this.toastMessage('Navegador Iniciado');
+        },
+        error => {
+          this.toastMessage(error);
+        }
+      );
+  }
+
+  toastMessage = (message: string) => {
+    this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    }).present();
+  }
 }
